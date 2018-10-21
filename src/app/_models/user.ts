d242848e69eafb0c2a15e67c35002f12379b3d6b
Xarq <stepnikpa@gmail.com>
2018-10-21 08:37:50 +0200

    Walidacja danych logowania

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -7,15 +7,55 @@ export class User
     public login: string;
     public password: string;
     public email: string;
-    public periodStart: Date;
-    public periodEnd: Date;
+    public periodStart: string;
+    public periodEnd: string;
 
-    public constructor(l: string, p: string, e: string, ps: Date, pe: Date)
+    public constructor(l: string, p: string, e: string, ps: string, pe: string)
     {
         this.login = l;
         this.password = p;
         this.email = e;
         this.periodStart = ps;
         this.periodEnd = pe;
+    }
+
+    // Metoda pobiera użytkownika na podstawie loginu
+    // Jeśli podano zły login lub użytkownik nie istnieje to metoda zwróci null
+    public getUser() : User
+    {
+        let example_users : User[] =
+        [
+           new User("Admin", "Admin", "Admin@mail.com", "20180101", "20180131"),
+           new User("Xarq", "Qrax", "Xarq@mail.com", "201801027", "20180226")
+        ]
+
+        let usr : User
+
+        for(let i = 0; i <= example_users.length - 1; i++)
+        {
+            if(example_users[i].login == this.login)
+            { usr = example_users[i]; break; } 
+            else 
+            { usr = null; continue; }
+        }
+
+        return usr;
+    }
+
+    // Metoda sprawdza czy podano prawidłowe hasło
+    public validateLogForm(dbUser: User) : boolean
+    {
+       let validationResult = this.password == dbUser.password ? true : false;
+
+       return validationResult;
+    }
+
+    // Metoda sprawdza czy dany użytkownik o danym loginie istnieje
+    // Jeśli tak to nie będzie możliwe zarejestrowanie użytkownika
+    public validateRegForm(formUser: User) : boolean
+    {
+        let checkUser = this.getUser()
+
+        return checkUser != null ? true : false;
     }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
-
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-login',
@@ -12,12 +12,30 @@ export class LoginComponent implements OnInit {
   constructor(private route: Router) 
   { }
 
-  user : any = { }
+  user : any = {};
+  userForm: User;
+  userBase: User;
+  checkValidation: boolean;
 
   ngOnInit() { }
 
+  // metoda odpowiada za obsługę przycisku logowania
   btnLogIn()
   {
-    this.route.navigate(['/details'])
+    // Etap 1
+    // stworzenie uzytkownika na podstawie danych formularza oraz
+    // drugiej zawierajace dane z bazy (o ile dany uzytkownik istnieje)
+    this.userForm = new User(this.user.login, this.user.password, "", "","");
+    this.userBase = this.userForm.getUser();
+
+    // Etap 2 - walidacja hasła (akcja się nie wykona jeśli podano zły lozgin lub użytkwonik nie istnieje)
+    if(this.userBase != null)  
+    {
+      this.checkValidation = this.userForm.validateLogForm(this.userBase);
+    }
+    else console.log("User does not exist");
+
+    console.log(this.userBase);
+    console.log(this.checkValidation)
   }
 }
